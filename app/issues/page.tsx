23bb71e7ae8eb5bcec6fb677caa8d0e04ddfc35a
dhughneil <dhughneil@protonmail.com>
2024-03-11 20:55:34 -0500

    Radix installed and applied

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import { Button } from "@radix-ui/themes";
 
 const IssuesPage = () => {
-  return <div>Original page</div>;
+  return <div><Button>New Issue</Button></div>;
 };
 
 export default IssuesPage;
