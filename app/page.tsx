b67122e5148dccc60d7a5663703071de1d5ec389
dhughneil <dhughneil@protonmail.com>
2024-03-11 21:21:25 -0500

    Built new issue page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <div className="px-5">Hello World!</div>;
+  // return <div className="px-5">Hello World!</div>;
+  return <div>Ticket Tracker Dashboard</div>;
 }
