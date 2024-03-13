d45d0cc174b9dad2ee30d13830cea9c1d267a2c6
dhughneil <dhughneil@protonmail.com>
2024-03-12 22:44:26 -0500

    Half way through the submit, errors still.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,19 +1,50 @@
-'use client'
-import { Button, TextField } from '@radix-ui/themes'
+"use client";
+import { Button, TextField } from "@radix-ui/themes"; // I'm didn't
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
 
-
-const NewIssuePage = () => {
-  return (
-    <div className='max-w-xl space-y-3'>
-        <TextField.Root>
-            <TextField.Input placeholder="Title" />
-        </TextField.Root>
-            <SimpleMDE placeholder="Enter a description of the issue here..." />
-            <Button>Submit new issue</Button>
-    </div>
-  )
+interface IssueForm {
+  title: string;
+  description: string;
 }
 
-export default NewIssuePage
+const NewIssuePage = () => {
+  const { register, control, handleSubmit, setValue } = useForm<IssueForm>();
+
+  const handleDescriptionChange = (value: string) => {
+    setValue("description", value);
+  };
+
+  const onSubmit = (data: IssueForm) => {
+    console.log(data);
+  };  
+
+  return (
+    <form
+      className="max-w-xl space-y-3"
+    //   onSubmit={handleSubmit((data) => console.log(data))}
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <TextField.Root>
+        <TextField.Input placeholder="Title" {...register("title")} />
+      </TextField.Root>
+      {/* <Controller
+        name="description"
+        control={control}
+        render={(field) => (
+          <SimpleMDE placeholder="Description..." {...field} />
+        )}
+      /> */}
+        <SimpleMDE
+        onChange={handleDescriptionChange}
+        options={{
+          placeholder: "Description...",
+        }}
+      />
+      <Button type="submit">Submit new issue</Button>
+    </form>
+  );
+};
+
+export default NewIssuePage;
