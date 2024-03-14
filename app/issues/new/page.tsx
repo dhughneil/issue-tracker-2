d69d4eccd29de1e5f4d0cc9c5c1432144f9de385
dhughneil <dhughneil@protonmail.com>
2024-03-13 19:39:42 -0500

    Updated the validation errors.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,20 +1,34 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes"; // I'm didn't
+import { Button, Callout, TextField, Text } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchemas";
+import { z } from "zod";
 
-interface IssueForm {
-  title: string;
-  description: string;
-}
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// The interface is being replaced by the type line above, pulling it from the validationSchema.
+// interface IssueForm {
+//   title: string;
+//   description: string;
+// }
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const { register, control, handleSubmit, setValue } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   const handleDescriptionChange = (value: string) => {
     setValue("description", value);
@@ -48,6 +62,11 @@ const NewIssuePage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
         {/* <Controller
         name="description"
         control={control}
@@ -60,11 +79,18 @@ const NewIssuePage = () => {
           options={{
             placeholder: "Description...",
           }}
-        />
+          />
+          {(errors.description && (
+            <Text color="red" as="p">
+              {errors.description.message}
+            </Text>
+          ))}
         <Button type="submit">Submit new issue</Button>
       </form>
     </div>
   );
 };
+
+// The Controller and simpleMDE tag above is what works. It was recommended by ChatGPT when Mosh's code simpley didn't work. The code does nothing when clicking the button. It matched Mosh's perfectly, but still failed. I commented it out so i'd have it. But since it wasn't working, I asked ChatGPT. The code that is not commented is based on it's recommendations.
 
 export default NewIssuePage;
